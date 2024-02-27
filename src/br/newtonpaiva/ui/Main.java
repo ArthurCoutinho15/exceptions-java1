5f488f6fa4095317766473610b417f422978b5be
Arthur Coutinho <arthurtelescoutinho@gmail.com>
2024-02-27 18:25:56 -0300

    feat: very bad solution

--- src/br/newtonpaiva/ui/Main.java
+++ src/br/newtonpaiva/ui/Main.java
@@ -42,6 +42,7 @@ public class Main {
                 reservation.updateDates(checkIn, checkOut);
                 System.out.println(reservation);
             }
+            int a;
         }
     }
 }
